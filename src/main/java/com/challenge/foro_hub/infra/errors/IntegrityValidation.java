f3fb5c7ee1fb8b3ccf7e0ec5b8598931f0818cdd
gabriel-gett <gett.912@gmail.com>
2025-08-19 23:04:40 -0600

    implementando package infra

---
+++ src/main/java/com/challenge/foro_hub/infra/errors/IntegrityValidation.java
@@ -0,0 +1,13 @@
+package com.challenge.foro_hub.infra.errors;
+
+import java.io.Serial;
+
+public class IntegrityValidation extends RuntimeException {
+    @Serial
+    private static final long serialVersionUID = 1L;
+    public IntegrityValidation(String s){
+        super(s);
+    }
+
+}
+
---
+++ src/main/java/com/challenge/foro_hub/infra/service/AuthenticationService.java
@@ -0,0 +1,21 @@
+package com.challenge.foro_hub.infra.service;
+
+
+import com.challenge.foro_hub.domain.usuario.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return usuarioRepository.findByUsername(username);
+    }
+}
